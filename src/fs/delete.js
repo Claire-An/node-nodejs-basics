a2700f56cdeba4b0715361957a73697838bec4a7
Svetlana <Claire-An@mail.ru>
2024-10-06 13:20:11 +0500

    feat: add solution Delete

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,20 @@
+import fs from 'node:fs';
+import { fileURLToPath } from 'node:url';
+import { dirname, join } from 'node:path';
+
 const remove = async () => {
-    // Write your code here 
+    const folderName = 'files';
+    const fileName = 'fileToRemove.txt';
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = dirname(__filename);
+    const filePath = join(__dirname, folderName, fileName);
+
+    fs.unlink(filePath, (err) => {
+        if(err) {
+            throw new Error('FS operation failed');
+        }
+    });
+
 };
 
 await remove();
